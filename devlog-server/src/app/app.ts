1ada84f3fd3bac815a6c8c9c36230e799b0eeaf1
Mufique Patwary <mdmusfiquepatwary@gmail.com>
2026-09-15 16:44:27 +0600

    added api documentation with swagger

--- devlog-server/src/app/app.ts
+++ devlog-server/src/app/app.ts
@@ -11,17 +11,20 @@ import { notFound } from "./middleware/notFound";
 import cron from "node-cron";
 import { inviteServices } from "./module/invite/invite.services";
 import path from "path";
-import { StandupLogServices } from "./module/standupLogs/standupLogs.services";
-import { prisma } from "../lib/prisma";
-import { sendEmail } from "./utils/sendEmail";
-import { getWeekRange } from "./utils/getWeekRange";
+import { fileURLToPath } from "url";
+import YAML from "yamljs";
+import swagger from "swagger-ui-express";
 
 import { paymentController } from "./module/payment/payment.controller";
 import { envVars } from "./config/env";
-import { PLAN } from "../generated/prisma/enums";
 import { initSocket } from "./utils/socket";
 
 dotenv.config();
+
+const currentDirectory = path.dirname(fileURLToPath(import.meta.url));
+const swaggerDocument = YAML.load(
+  path.join(currentDirectory, "docs", "swagger.yaml"),
+);
 
 const app = express();
 const server = http.createServer(app);
@@ -44,7 +47,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.set("view engine", "ejs");
-app.set("views", path.resolve(process.cwd(), "src/app/templates"));
 
 app.use(
   cors({
@@ -54,6 +56,8 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+app.use("/api/docs", swagger.serve, swagger.setup(swaggerDocument));
 
 app.use("/api/auth", toNodeHandler(auth));
 
